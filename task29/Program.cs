9a2192a1c770fa96f618071f02e9e2412ffc1c95
Aleksandr Volkov <222volkov@gmail.com>
2023-01-25 17:15:24 +0300

    task29 done

---
+++ task29/Program.cs
@@ -0,0 +1,53 @@
+﻿// курс "Знакомство с языками программирования" вебинар 4 домашнее задание
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов
+// и выводит их на экран
+// 1, 2, 5, 7, 19  -> [1, 2, 5, 7, 19]
+// 6, 1, 33        -> [6, 1, 33]   
+
+
+// 1. Логика функции
+// 2. Ввод данных с проверкой
+// 3. Демонстрация работы функции с выводом в консоль
+// 4.  Немного интерфейса))
+
+// 1. Функция заполнения массива
+int[] FillArray(int arraySize)
+{
+    int[] result = new int[arraySize];
+    Random tmp = new Random();
+    for(int i = 0; i < result.Length; i++) {
+        result[i] = tmp.Next(1, 99);
+    }
+    return result;
+}
+
+// 2. Функция вывода массива в консоль
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for(int i = 0; i < array.Length - 1; i++) {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.WriteLine($"{array[array.Length - 1]}]");
+}
+
+string exit_condition = String.Empty;
+while (exit_condition != "n")
+{
+    // 2. Ввод данных с проверкой
+    Console.WriteLine("Программа задаёт массив из 8 элементов и выводит их на экран.");
+    System.Console.Write("Но можно задать любую размерность массива, введите количество элементов массива:  ");
+    int.TryParse(Console.ReadLine(), out int number);
+
+    // 3. Демонстрация работы функций с выводом в консоль
+    int[] result = FillArray(number);
+    PrintArray(result);
+    System.Console.WriteLine();
+    // 4.  Немного интерфейса))
+    System.Console.Write("Хотите сделать это ещё раз? y / n  ");
+    exit_condition = Console.ReadLine();
+}
+
+System.Console.WriteLine("Благодарим за использование программы!");
+
